910b4c3daf74e8b83c19b90ebd2cabc1d9f53939
avaer <avaer@users.noreply.github.com>
2014-10-14 09:06:58 -0400

    Update test.js

--- test.js
+++ test.js
@@ -1,13 +1,13 @@
 /* test.js */
 var http = require('http'),
-request = require('request'),
-should = require('should'),
-app = require('./app.js');
+    request = require('request'),
+    should = require('should'),
+    app = require('./app.js');
 
 describe('basic', function() {
     it('should respond with hello world', function(cb) {
         var s = app(function(err) {
-            http.get('http://127.0.0.1:8080', function(err, body) {
+            request('http://127.0.0.1:8080', function(err, res, body) {
                 body.should.be.exactly('Hello, world!');
 
                 s.close(cb);
